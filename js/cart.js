d7d888cdacdd8f344bcc433f9346237a84c956b4
StarcevAlexander <116572797+StarcevAlexander@users.noreply.github.com>
2024-09-19 12:43:15 +0800

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,39 +1,39 @@
-import { calcCartPriceAndDelivery } from "./calcCartPrice.js";
+import {calcCartPriceAndDelivery} from "./calcCartPrice.js";
 
 // Div внутри корзины, в который мы добавляем товары
-const cartWrapper =  document.querySelector('.cart-wrapper');
+const cartWrapper = document.querySelector('.cart-wrapper');
 
 // Отслеживаем клик на странице
 window.addEventListener('click', function (event) {
-	// Проверяем что клик был совершен по кнопке "Добавить в корзину"
-	if (event.target.hasAttribute('data-cart')) {
+    // Проверяем что клик был совершен по кнопке "Добавить в корзину"
+    if (event.target.hasAttribute('data-cart')) {
 
-		// Находим карточку с товаром, внутри котрой был совершен клик
-		const card = event.target.closest('.card');
+        // Находим карточку с товаром, внутри котрой был совершен клик
+        const card = event.target.closest('.card');
 
-		// Собираем данные с этого товара и записываем их в единый объект productInfo
-		const productInfo = {
-			id: card.dataset.id,
-			imgSrc: card.querySelector('.product-img').getAttribute('src'),
-			title: card.querySelector('.item-title').innerText,
-			itemsInBox: card.querySelector('[data-items-in-box]').innerText,
-			weight: card.querySelector('.price__weight').innerText,
-			price: card.querySelector('.price__currency').innerText,
-			counter: card.querySelector('[data-counter]').innerText,
-		};
+        // Собираем данные с этого товара и записываем их в единый объект productInfo
+        const productInfo = {
+            id: card.dataset.id,
+            imgSrc: card.querySelector('.product-img').getAttribute('src'),
+            title: card.querySelector('.item-title').innerText,
+            itemsInBox: card.querySelector('[data-items-in-box]').innerText,
+            weight: card.querySelector('.price__weight').innerText,
+            price: card.querySelector('.price__currency').innerText,
+            counter: card.querySelector('[data-counter]').innerText,
+        };
+        console.log(productInfo)
+        // Проверять если ли уже такой товар в корзине
+        const itemInCart = cartWrapper.querySelector(`[data-id="${productInfo.id}"]`);
 
-		// Проверять если ли уже такой товар в корзине
-		const itemInCart = cartWrapper.querySelector(`[data-id="${productInfo.id}"]`);
+        // Если товар есть в корзине
+        if (itemInCart) {
+            const counterElement = itemInCart.querySelector('[data-counter]');
+            counterElement.innerText = parseInt(counterElement.innerText) + parseInt(productInfo.counter);
+        } else {
+            // Если товара нет в корзине
 
-		// Если товар есть в корзине
-		if (itemInCart) {
-			const counterElement = itemInCart.querySelector('[data-counter]');
-			counterElement.innerText = parseInt(counterElement.innerText) + parseInt(productInfo.counter);
-		} else {
-			// Если товара нет в корзине
-
-			// Собранные данные подставим в шаблон для товара в корзине
-			const cartItemHTML = `<div class="cart-item" data-id="${productInfo.id}">
+            // Собранные данные подставим в шаблон для товара в корзине
+            const cartItemHTML = `<div class="cart-item" data-id="${productInfo.id}">
 								<div class="cart-item__top">
 									<div class="cart-item__img">
 										<img src="${productInfo.imgSrc}" alt="${productInfo.title}">
@@ -63,18 +63,18 @@ window.addEventListener('click', function (event) {
 								</div>
 							</div>`;
 
-			// Отобразим товар в корзине
-			cartWrapper.insertAdjacentHTML('beforeend', cartItemHTML);
-		}
+            // Отобразим товар в корзине
+            cartWrapper.insertAdjacentHTML('beforeend', cartItemHTML);
+        }
 
-		// Сбрасываем счетчик добавленного товара на "1"
-		card.querySelector('[data-counter]').innerText = '1';
+        // Сбрасываем счетчик добавленного товара на "1"
+        card.querySelector('[data-counter]').innerText = '1';
 
-		// Отображение статуса корзины Пустая / Полная
-		toggleCartStatus();
+        // Отображение статуса корзины Пустая / Полная
+        toggleCartStatus();
 
-		// Пересчет общей стоимости товаров в корзине
-		calcCartPriceAndDelivery();
+        // Пересчет общей стоимости товаров в корзине
+        calcCartPriceAndDelivery();
 
-	}
+    }
 });
